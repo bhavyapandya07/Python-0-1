a896a8f453bc35f92086e2d447a67927edf79938
Bhavya Pandya <bhavyapandya07@gmail.com>
2024-09-11 19:26:47 +0530

    test

--- Lecture 25 Plotting/Lec25.py
+++ Lecture 25 Plotting/Lec25.py
@@ -35,3 +35,19 @@ plt.xlabel('Test Values')
 plt.ylabel('Squared Values')
 plt.title('Test Values vs. Their Squares')
 plt.show()
+
+#----
+
+import matplotlib.pyplot as plt
+
+mon = range(1, 13, 1)
+bost = [28, 32, 39, 48, 59, 68, 75, 73, 66, 54, 45, 34]
+plt.plot(mon, bost, label = 'boston')
+phoe = [54, 57, 61, 68, 77, 86, 91, 90, 84, 73, 61, 54]
+plt.plot(mon, phoe, label = 'phoenix')
+plt.title('avg. temp')
+plt.xlabel('months')
+plt.ylabel('degrees F')
+plt.legend(loc = 'best')
+plt.grid()
+plt.show()
